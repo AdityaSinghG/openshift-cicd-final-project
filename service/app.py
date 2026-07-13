a48dc4a79fbd29aa5cd8d82bb0e1bbb589c3aa82
ADITYA SINGH GAHARWAR <adityasingh70717@gmail.com>
2026-07-14 02:08:37 +0530

    Update app.py

--- service/app.py
+++ service/app.py
@@ -2,6 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
+    """Return a simple greeting."""
     return "Hello, CI/CD World!"
+
+
+if __name__ == "__main__":
+    app.run()
